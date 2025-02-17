19058296694e9af278f7286593ea0d8f8a971130
FeetusDeletus12 <braxton.gough0@gmail.com>
2025-02-17 14:43:51 +1100

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// script.js
+
+// You can add custom JS here if you want to modify the behavior of the page.
+// Right now, this file can be left empty since Gradio.js handles most of the functionality.
