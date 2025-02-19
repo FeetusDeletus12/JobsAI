9f4af9634d8de13d87258ecc860622d7094c794d
FeetusDeletus12 <braxton.gough0@gmail.com>
2025-02-19 19:04:12 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,1 @@
-// script.js
 
-// You can add custom JS here if you want to modify the behavior of the page.
-// Right now, this file can be left empty since Gradio.js handles most of the functionality.
